33eeedeb711f2f24a0b30d9a1bef07eb33ad7481
이영재 <dudwo1830@naver.com>
2026-01-19 11:00:45 +0900

    [chore] 프로젝트 생성

---
+++ src/test/java/net/datasa/EnLink/EnLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.datasa.EnLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
