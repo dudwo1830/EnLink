a2da2ffe725d14823fee10746e883b487f43f0bd
이영재 <dudwo1830@naver.com>
2026-01-23 08:51:03 +0900

    [fix] 비밀번호 암호화 처리 오류 수정

--- src/main/java/net/datasa/EnLink/config/SecurityConfig.java
+++ src/main/java/net/datasa/EnLink/config/SecurityConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		// BCrypt 알고리즘 사용
+		return new BCryptPasswordEncoder();
+	}
 
 	/**
 	 * 프로젝트 초기 세팅
