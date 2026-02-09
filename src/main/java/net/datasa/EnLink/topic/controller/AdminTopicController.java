be1c08888a412c4a9c5ea80453b6ecd2710dc24b
이영재 <dudwo1830@naver.com>
2026-02-09 11:44:44 +0900

    [fix] 관리자 - 주제 관리 오류 수정

--- src/main/java/net/datasa/EnLink/topic/controller/AdminTopicController.java
+++ src/main/java/net/datasa/EnLink/topic/controller/AdminTopicController.java
@@ -2,18 +2,26 @@ package net.datasa.EnLink.topic.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import net.datasa.EnLink.topic.service.TopicService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("admin/topics")
+@RequiredArgsConstructor
 public class AdminTopicController {
 	private final String TEMPLATE_BASE = "admin/";
+	private final TopicService topicService;
 
 	@GetMapping("")
 	@PreAuthorize("isAuthenticated() and hasRole('ADMIN')")
-	public String topic() {
-		return TEMPLATE_BASE + "topic";
+	public String topic(Model model) {
+		model.addAttribute("topics", topicService.getListAll());
+		return TEMPLATE_BASE + "topic/list";
 	}
 
 }
